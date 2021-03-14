915a0dcb7e6be0e024e980c38ef6a9463673e056
geraldo <geeferreirasd034@gmail.com>
2021-03-14 14:41:18 -0300

    stylizing navigation bar

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = 'senha-muito-forte'
+
+DEBUG = 'True'
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from app import app
 # export FLASK_APP=main.py
 # export FLASK_ENV=development
 # flask run
+#python3 main.py
+
 
 if __name__ == '__main__':
     app.run(debug=True)
