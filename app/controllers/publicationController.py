561a73591de4ee5fd738d556e1a97325e1fc3194
geraldo <geeferreirasd034@gmail.com>
2021-03-21 08:51:59 -0300

    correction of display image template of publication details

--- app/controllers/publicationController.py
+++ app/controllers/publicationController.py
@@ -4,7 +4,7 @@ from app import (
     app, 
     request, 
     render_template, 
-    redirect, 
+    redirect, url_for,
     os)
 from app.models.Publication import PublicationModel
 from app.forms import PublicationForm
@@ -23,7 +23,7 @@ def list_publication():
     publications = PublicationModel.query.all()
     return render_template('publications/list-publication.html', publications=publications)
 
-@app.route('/publication/new-publication', methods=['GET', 'POST'])
+@app.route('/publication/new', methods=['GET', 'POST'])
 def create_publication():
     
     form = PublicationForm()
@@ -54,7 +54,7 @@ def create_publication():
             new_publication.user_id = 1
             
             new_publication.save_publication()
-            return redirect('/')
+            return redirect(url_for('list_publication'))
     return render_template('publications/create-publication.html', form=form)
 
 @app.route('/publication/<int:id>')
@@ -62,43 +62,43 @@ def detail_publication(id):
     publication = PublicationModel.find_by_publication(id=id)
     if publication:
         return render_template('publications/detail-publication.html', publication=publication)
-    return redirect('/')
+    return redirect(url_for('list_publication'))
 
 @app.route('/publication/delete/<int:id>')
 def delete_publication(id):
     publication = PublicationModel.query.get(id)
     if publication:
         publication.delete_publication()
-        return  redirect('/')
-    return redirect('/')
+        return  redirect(url_for('list_publication'))
+    return redirect(url_for('list_publication'))
 
-@app.route('/publication/update/<int:id>', methods=['GET', 'POST'])
-def update_publication(id):
+# @app.route('/publication/update/<int:id>', methods=['GET', 'POST'])
+# def update_publication(id):
     
-    form = PublicationForm()
-    publication = PublicationModel().find_by_publication(id=id)
+#     form = PublicationForm()
+#     publication = PublicationModel().find_by_publication(id=id)
     
-    if request.method == 'POST':
-        #getting files file
-        #taking form data and turning it into dictionaries
-        file = request.files['photo']
-        data = request.form.to_dict() 
-        data['photo'] = file.filename
+#     if request.method == 'POST':
+#         #getting files file
+#         #taking form data and turning it into dictionaries
+#         file = request.files['photo']
+#         data = request.form.to_dict() 
+#         data['photo'] = file.filename
 
-        #validated form fields
-        # form = PublicationForm(**data)        
-        # if form.validate():
-            # new_publication = PublicationModel().find_by_publication(id=id)
+#         #validated form fields
+#         # form = PublicationForm(**data)        
+#         # if form.validate():
+#             # new_publication = PublicationModel().find_by_publication(id=id)
             
-            # #path and name of the file to be saved
-            # #saving file in the uploads directory 
-            # save_path = os.path.join(
-            #     UPLOAD_FOLDER, secure_filename(file.filename))
-            # file.save(save_path)
+#             # #path and name of the file to be saved
+#             # #saving file in the uploads directory 
+#             # save_path = os.path.join(
+#             #     UPLOAD_FOLDER, secure_filename(file.filename))
+#             # file.save(save_path)
             
-            # #logged in user
-            # new_publication.user_id = 1
+#             # #logged in user
+#             # new_publication.user_id = 1
             
-            # new_publication.save_publication()
-        return redirect('/')
-    return render_template('publications/create-publication.html', form=form, publication=publication)
+#             # new_publication.save_publication()
+#         return redirect('/')
+#     return render_template('publications/create-publication.html', form=form, publication=publication)
